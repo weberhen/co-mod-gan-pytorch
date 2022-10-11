b579bffa1e5a0fca694285c2618e2bbe61c893d0
henrique weber <iqueweber@gmail.com>
2022-10-11 17:37:51 +0000

    masks for crop generator

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@ python test.py \
 	--mixing 0 \
 	--batchSize 1 \
 	--nThreads 1 \
-	--name comod-places-512 \
+	--name comod_places \
 	--dataset_mode testimage \
 	--image_dir /root/datasets_raid/zillow/panos \
 	--mask_dir /root/datasets_raid/zillow/tripod_masks \
         --output_dir ./zillow \
-	--load_size 512 \
-	--crop_size 512 \
+	--load_size 128 \
+	--crop_size 128 \
 	--z_dim 512 \
 	--model comod \
 	--netG comodgan \
-	--which_epoch co-mod-gan-places2-050000 \
+	--which_epoch epoch18_step490002 \
 	--preprocess_mode resize \
 	${EXTRA} \
