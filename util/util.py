da3e717bc55149afecbf870e842a2745f9ddd257
henrique weber <iqueweber@gmail.com>
2023-03-30 16:38:37 -0400

    cleanup util

--- util/util.py
+++ util/util.py
@@ -6,11 +6,7 @@ print('Loading util/util.py')
 import re
 import importlib
 import torch
-import numpy as np
-from PIL import Image
 import os
-import argparse
-import dill as pickle
 import torchvision
 
 def display_batch(epoch, data_i, trainer, writer, iter_counter):
@@ -23,137 +19,6 @@ def display_batch(epoch, data_i, trainer, writer, iter_counter):
     infer_out = (infer_out+1)/2
     infer_out = infer_out.clamp(0,1)
     torchvision.utils.save_image(infer_out[:num_print], 'output.png', nrow=1, normalize=True, range=(0,1))
-
-def save_obj(obj, name):
-    with open(name, 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-
-def load_obj(name):
-    with open(name, 'rb') as f:
-        return pickle.load(f)
-
-# returns a configuration for creating a generator
-# |default_opt| should be the opt of the current experiment
-# |**kwargs|: if any configuration should be overriden, it can be specified here
-
-
-def copyconf(default_opt, **kwargs):
-    conf = argparse.Namespace(**vars(default_opt))
-    for key in kwargs:
-        print(key, kwargs[key])
-        setattr(conf, key, kwargs[key])
-    return conf
-
-
-def tile_images(imgs, picturesPerRow=4):
-    """ Code borrowed from
-    https://stackoverflow.com/questions/26521365/cleanly-tile-numpy-array-of-images-stored-in-a-flattened-1d-format/26521997
-    """
-
-    # Padding
-    if imgs.shape[0] % picturesPerRow == 0:
-        rowPadding = 0
-    else:
-        rowPadding = picturesPerRow - imgs.shape[0] % picturesPerRow
-    if rowPadding > 0:
-        imgs = np.concatenate([imgs, np.zeros((rowPadding, *imgs.shape[1:]), dtype=imgs.dtype)], axis=0)
-
-    # Tiling Loop (The conditionals are not necessary anymore)
-    tiled = []
-    for i in range(0, imgs.shape[0], picturesPerRow):
-        tiled.append(np.concatenate([imgs[j] for j in range(i, i + picturesPerRow)], axis=1))
-
-    tiled = np.concatenate(tiled, axis=0)
-    return tiled
-
-
-# Converts a Tensor into a Numpy array
-# |imtype|: the desired type of the converted numpy array
-def tensor2im(image_tensor, imtype=np.uint8, normalize=True, tile=False):
-    if isinstance(image_tensor, list):
-        image_numpy = []
-        for i in range(len(image_tensor)):
-            image_numpy.append(tensor2im(image_tensor[i], imtype, normalize))
-        return image_numpy
-
-    if image_tensor.dim() == 4:
-        # transform each image in the batch
-        images_np = []
-        for b in range(image_tensor.size(0)):
-            one_image = image_tensor[b]
-            one_image_np = tensor2im(one_image)
-            images_np.append(one_image_np.reshape(1, *one_image_np.shape))
-        images_np = np.concatenate(images_np, axis=0)
-        if tile:
-            images_tiled = tile_images(images_np)
-            return images_tiled
-        else:
-            return images_np
-
-    if image_tensor.dim() == 2:
-        image_tensor = image_tensor.unsqueeze(0)
-    image_numpy = image_tensor.detach().cpu().float().numpy()
-    if normalize:
-        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
-    else:
-        image_numpy = np.transpose(image_numpy, (1, 2, 0)) * 255.0
-    image_numpy = np.clip(image_numpy, 0, 255)
-    if image_numpy.shape[2] == 1:
-        image_numpy = image_numpy[:, :, 0]
-    return image_numpy.astype(imtype)
-
-
-def save_image(image_numpy, image_path, create_dir=False):
-    if create_dir:
-        os.makedirs(os.path.dirname(image_path), exist_ok=True)
-    if len(image_numpy.shape) == 2:
-        image_numpy = np.expand_dims(image_numpy, axis=2)
-    if image_numpy.shape[2] == 1:
-        image_numpy = np.repeat(image_numpy, 3, 2)
-    image_pil = Image.fromarray(image_numpy)
-
-    # save to png
-    image_pil.save(image_path.replace('.jpg', '.png'))
-
-
-def mkdirs(paths):
-    if isinstance(paths, list) and not isinstance(paths, str):
-        for path in paths:
-            mkdir(path)
-    else:
-        mkdir(paths)
-
-
-def mkdir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-
-def atoi(text):
-    return int(text) if text.isdigit() else text
-
-
-def natural_keys(text):
-    '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
-    '''
-    return [atoi(c) for c in re.split('(\d+)', text)]
-
-
-def natural_sort(items):
-    items.sort(key=natural_keys)
-
-
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 def find_class_in_module(target_cls_name, module):
@@ -206,13 +71,3 @@ def load_network(net, label, epoch, opt):
         new_dict[k] = v
     net.load_state_dict(new_dict, strict=False)
     return net
-
-
-###############################################################################
-# Code from
-# https://github.com/ycszen/pytorch-seg/blob/master/transform.py
-# Modified so it complies with the Citscape label map colors
-###############################################################################
-def uint82bin(n, count=8):
-    """returns the binary of integer n, count refers to amount of bits"""
-    return ''.join([str((n >> y) & 1) for y in range(count - 1, -1, -1)])
