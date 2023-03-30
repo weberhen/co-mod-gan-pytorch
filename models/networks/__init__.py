c0935347c6844f63519fd286ec77fb7e49f6ca7a
henrique weber <iqueweber@gmail.com>
2023-03-30 16:46:28 -0400

    remove init_weights

--- models/networks/__init__.py
+++ models/networks/__init__.py
@@ -9,7 +9,6 @@ from models.networks.loss import *
 from models.networks.discriminator import *
 from models.networks.generator import *
 import util.util as util
-import pdb
 
 
 def find_network_using_name(target_network_name, filename):
--- models/networks/base_network.py
+++ models/networks/base_network.py
@@ -15,39 +15,6 @@ class BaseNetwork(nn.Module):
     def modify_commandline_options(parser, is_train):
         return parser
 
-    def init_weights(self, init_type='normal', gain=0.02):
-        def init_func(m):
-            classname = m.__class__.__name__
-            if classname.find('BatchNorm2d') != -1:
-                if hasattr(m, 'weight') and m.weight is not None:
-                    init.normal_(m.weight.data, 1.0, gain)
-                if hasattr(m, 'bias') and m.bias is not None:
-                    init.constant_(m.bias.data, 0.0)
-            elif hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
-                if init_type == 'normal':
-                    init.normal_(m.weight.data, 0.0, gain)
-                elif init_type == 'xavier':
-                    init.xavier_normal_(m.weight.data, gain=gain)
-                elif init_type == 'xavier_uniform':
-                    init.xavier_uniform_(m.weight.data, gain=1.0)
-                elif init_type == 'kaiming':
-                    init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-                elif init_type == 'orthogonal':
-                    init.orthogonal_(m.weight.data, gain=gain)
-                elif init_type == 'none':  # uses pytorch's default init method
-                    m.reset_parameters()
-                else:
-                    raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
-                if hasattr(m, 'bias') and m.bias is not None:
-                    init.constant_(m.bias.data, 0.0)
-
-        self.apply(init_func)
-
-        # propagate to children
-        for m in self.children():
-            if hasattr(m, 'init_weights'):
-                m.init_weights(init_type, gain)
-
     def get_param_list(self, label):
         print("updating all params")
         return self.parameters()
